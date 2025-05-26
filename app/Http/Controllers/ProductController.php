a7d8295531fe5e136e41ee486708ca9544a0fcab
serrera-lc <195297216+serrera-lc@users.noreply.github.com>
2025-05-27 00:30:45 +0800

    modified product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -55,18 +55,10 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        // Try to find as a product first
-        $product = Product::find($id);
-        if ($product) {
-            return response()->json($product, 200);
-        }
-
-        // If not found as a product, try as a user_id
         $userProducts = Product::where('user_id', $id)->get();
         if ($userProducts->count() > 0) {
             return response()->json(['data' => $userProducts], 200);
         }
-
         return response()->json(['message' => 'Product(s) not found'], 404);
     }
 
